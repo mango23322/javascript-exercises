9b1baddad9efd14a0b492ee99eb136a73a7b093f
Kevin Mengarelli <mango23322@gmail.com>
2022-08-27 14:17:30 -0500

    completed  exercises 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    var newstring = '';
+    for (i = 0; i < num; i++) {
+        newstring = newstring + string;
+    }
+    
+    if (num < 0) {
+        newstring = 'ERROR'
+    }
 
+    return newstring
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(regstring) {
+    var newstring = '';
 
+    for (let i = 1; i <= regstring.length; i++) {
+        newstring = newstring.concat(regstring.charAt(regstring.length - i));
+    }
+
+    return newstring;
 };
 
 // Do not edit below this line
