791afa510323fbe98b07b377e9ff6cbb2349d26c
Kevin Mengarelli <mango23322@gmail.com>
2022-08-28 22:26:20 -0500

    completed calculator (08)

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return (x + y);
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return (x - y)
 };
 
-const sum = function() {
-	
+const sum = function(x) {
+  let sum = 0;
+	for (let i = 0; i < x.length; i++) {
+    sum += x[i];
+  }
+  return sum
 };
 
-const multiply = function() {
+const multiply = function(x) {
 
+  let result = x[0];
+  for (let i = 1; i < x.length; i++) {
+    result = result * x[i];
+  } 
+  return (result)
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  result = x;
+
+  if (y == 0) {
+    result = 1;
+  }
+
+  for (let i = 2; i <= y; i++) {
+    result = result * x;
+  }
+
+	return result
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let result = 1;
+
+  for (let i = 1; i <= x; i++) {
+    result = result * i;
+  }
+	return result
 };
 
 // Do not edit below this line
