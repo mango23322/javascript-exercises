aa559963502956bf29b796eecbdf6f74eb3616c5
Kevin Mengarelli <mango23322@gmail.com>
2022-08-28 21:41:43 -0500

    finished ex 05 sumall

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    xy = 0;
+    //console.log(typeof(x));
 
+    if (x < 0 || y < 0) {
+        return 'ERROR'
+    }
+    if ( (typeof(x) != 'number') || (typeof(y) != 'number')) {
+        return 'ERROR'
+    }
+
+    if (x < y ){
+        while (x <= y) {
+            xy += x;
+            x++
+        }    
+    }
+    else {
+        while (y <= x) {
+            xy += y;
+            y++
+        }   
+    }
+
+    return xy
 };
 
 // Do not edit below this line
+sumAll(5,6);
 module.exports = sumAll;
