04787237150c40bbc650f0e2aa0762792b568eeb
Kevin Mengarelli <mango23322@gmail.com>
2022-08-29 17:28:13 -0500

    finished palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let newstring = '';
+    let reverse = '';
+ //   newstring = input.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g,"");
+    console.log(newstring);
+    reverse = newstring.split("").reverse().join("");
+   // console.log(reverse);
+    if (newstring.toLowerCase() === reverse.toLowerCase) {
+        return true
+    }
+    return false
 };
 
 // Do not edit below this line
